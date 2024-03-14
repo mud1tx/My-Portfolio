466f1b67590ea822d97fba69ef9a768680f74098
mud1tx <muditagarwalna@gmail.com>
2024-03-14 22:36:26 +0530

    main page configured with components

--- app/(Home)/page.tsx
+++ app/(Home)/page.tsx
@@ -1,10 +1,18 @@
 import React from "react";
 import Navbar from "../components/Navbar";
+import HeroPage from "./Hero";
+import Footer from "../components/Footer";
+import WhatIDoPage from "./WhatIDo";
+import Skills from "./Skills";
 
 const HomePage = () => {
   return (
     <div>
       <Navbar />
+      <HeroPage />
+      <WhatIDoPage />
+      <Skills />
+      {/* <Footer /> */}
     </div>
   );
 };

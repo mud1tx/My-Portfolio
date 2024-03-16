fdda2579ecd59012e6f46ab8ac85e4c91fd13a8f
mud1tx <muditagarwalna@gmail.com>
2024-03-16 23:03:04 +0530

    optimized skill component

--- app/(Home)/Skills.tsx
+++ app/(Home)/Skills.tsx
@@ -2,7 +2,40 @@ import Image from "next/image";
 import React from "react";
 import Marquee from "react-fast-marquee";
 
+type Skill = {
+  src: string;
+  alt: string;
+};
+
 const Skills = () => {
+  const FRONTEND: Skill[] = [
+    { src: "html-5.png", alt: "HTML" },
+    { src: "css.png", alt: "CSS" },
+    { src: "javascript.svg", alt: "Javascript" },
+    { src: "java.png", alt: "Java" },
+    { src: "typescript.png", alt: "Typescript" },
+    { src: "bootstrap.svg", alt: "Bootstrap" },
+    { src: "react.png", alt: "React" },
+    { src: "redux.svg", alt: "Redux" },
+    { src: "next.png", alt: "Next" },
+    { src: "tailwind.png", alt: "Tailwind" },
+    { src: "git.svg", alt: "Git" },
+    { src: "github.svg", alt: "Github" },
+  ];
+
+  const BACKEND: Skill[] = [
+    { src: "node-js.png", alt: "HTML" },
+    { src: "express.svg", alt: "CSS" },
+    { src: "mongodb.svg", alt: "Javascript" },
+    { src: "postgres.png", alt: "Java" },
+    { src: "linux.png", alt: "Typescript" },
+    { src: "jenkins.png", alt: "Bootstrap" },
+    { src: "nginx.svg", alt: "React" },
+    { src: "Docker.svg", alt: "Redux" },
+    { src: "aws.png", alt: "Tailwind" },
+    { src: "graphql.svg", alt: "Git" },
+  ];
+
   return (
     <div>
       <main className="flex flex-col justify-center w-full m-auto mt-14 max-w-13xl">
@@ -19,69 +52,35 @@ const Skills = () => {
             gradientColor="#0e100f"
             speed={30}
           >
-            <div className="bg-primaryBlackCard m-1 rounded-xl p-4 cursor-pointer">
-              <Image
-                src="/html.svg"
-                alt="Html"
-                width={50}
-                height={50}
-              />
-            </div>
-            <div className="bg-primaryBlackCard m-1 rounded-xl p-4 cursor-pointer">
-              <Image
-                src="/css.svg"
-                alt="Css"
-                width={50}
-                height={50}
-              />
-            </div>
-            <div className="bg-primaryBlackCard m-1 rounded-xl p-4 cursor-pointer">
-              <Image
-                src="/javascript.svg"
-                alt="Html"
-                width={50}
-                height={50}
-              />
-            </div>
-            <div className="bg-primaryBlackCard m-1 rounded-xl p-4 cursor-pointer">
-              <Image
-                src="/javascript.svg"
-                alt="Html"
-                width={50}
-                height={50}
-              />
-            </div>
-            <div className="bg-[#202020] m-1 rounded-xl flex space-x-5 p-4 font-bold text-2xl cursor-pointer text-sky-400">
-              HTML
-            </div>
-            <div className="bg-[#202020] m-1 rounded-xl flex space-x-5 p-4 font-bold text-2xl cursor-pointer text-sky-400">
-              JAVASCRIPT
-            </div>
+            {FRONTEND.map((img) => (
+              <div className="bg-primaryBorder m-1 rounded-xl p-4 cursor-pointer">
+                <Image
+                  src={"/" + img.src}
+                  alt={img.alt}
+                  width={50}
+                  height={50}
+                />
+              </div>
+            ))}
           </Marquee>
           <Marquee
             autoFill
             pauseOnHover
             direction="right"
             speed={30}
+            gradient
+            gradientColor="#0e100f"
           >
-            <div className="bg-[#202020] m-1 rounded-xl flex space-x-5 p-4 font-bold text-2xl cursor-pointer text-sky-400">
-              TailWindCSS
-            </div>
-            <div className="bg-[#202020] m-1 rounded-xl flex space-x-5 p-4 font-bold text-2xl cursor-pointer text-sky-400">
-              Next
-            </div>
-            <div className="bg-[#202020] m-1 rounded-xl flex space-x-5 p-4 font-bold text-2xl cursor-pointer text-sky-400">
-              React
-            </div>
-            <div className="bg-[#202020] m-1 rounded-xl flex space-x-5 p-4 font-bold text-2xl cursor-pointer text-sky-400">
-              Java
-            </div>
-            <div className="bg-[#202020] m-1 rounded-xl flex space-x-5 p-4 font-bold text-2xl cursor-pointer text-sky-400">
-              HTML
-            </div>
-            <div className="bg-[#202020] m-1 rounded-xl flex space-x-5 p-4 font-bold text-2xl cursor-pointer text-sky-400">
-              JAVASCRIPT
-            </div>
+            {BACKEND.map((img) => (
+              <div className="bg-primaryBorder m-1 rounded-xl p-4 cursor-pointer">
+                <Image
+                  src={"/" + img.src}
+                  alt={img.alt}
+                  width={50}
+                  height={50}
+                />
+              </div>
+            ))}
           </Marquee>
         </main>
       </main>
